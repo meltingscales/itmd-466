e6d7d7aaeb3060a0d5a1d75cb5658c86c511ff52
Henry Post <HenryFBP@gmail.com>
2019-03-08 20:31:25 -0600

    add test to insert message

--- assignments/setup-jax-rs/henry/src/test/java/org/henrypost/itm466/CoolTest.java
+++ assignments/setup-jax-rs/henry/src/test/java/org/henrypost/itm466/CoolTest.java
@@ -1,10 +1,13 @@
 package org.henrypost.itm466;
 
 import junit.framework.TestCase;
+import org.henrypost.itm466.model.Message;
 import org.junit.Test;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -16,25 +19,36 @@ import static org.valid4j.matchers.http.HttpResponseMatchers.hasStatus;
 
 public class CoolTest extends TestCase {
 
+    static int port = 8080;
+    static String scheme = "http";
+    static String host = "localhost";
+    static String endpoint = "henry/webapi";
 
-    static String baseURL = "http://localhost:8080/henry/webapi";
+    static String baseURL = String.format("%s://%s:%d/%s", scheme, host, port, endpoint);
 
-    @Override
-    protected void setUp() {
+    @Test
+    public void testSimple() {
+        Client client = ClientBuilder.newClient();
+
+        Response response = client.target(String.format("%s/myresource", baseURL)).request(MediaType.TEXT_PLAIN).get();
+
+        assertThat(response, hasStatus(Response.Status.OK));
+        assertThat(response, hasEntity(equalTo("Got it!")));
 
     }
 
     @Test
-    public void testSimple() {
-
-        // Invoke your web service using plain JAX-RS. E.g:
+    public void testInsertMessage() {
         Client client = ClientBuilder.newClient();
-        Response response = client.target(String.format("%s/myresource", baseURL)).request("text/plain").get();
 
-        // Verify the response
-        assertThat(response, hasStatus(Response.Status.OK));
-        assertThat(response, hasEntity(equalTo("Got it!")));
+        Message message = new Message(-1L, "Testing!", "tester");
 
+        Entity<Message> messageEntity = Entity.json(message);
+
+        Response response = client.target(String.format("%s/message", baseURL)).request(MediaType.APPLICATION_JSON).post(messageEntity);
+
+        assertThat(response, hasEntity());
+        assertEquals(response.getStatus(), Response.Status.OK.getStatusCode());
     }
 
 }
